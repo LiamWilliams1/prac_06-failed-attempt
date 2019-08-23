49da865da0139db4a051eca36cb78ecea509e9b8
LiamWilliams1 <liam.williams@my.jcu.edu.au>
2019-08-23 11:07:51 +1000

    refactor into 2 functions

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -12,13 +12,16 @@ def main():
     for month in range(1, number_of_months + 1):
         income = float(input("Enter income for month {}: ".format(month)))
         incomes.append(income)
+    print_report(incomes, number_of_months)
 
+def print_report(incomes, number_of_months):
+    """prints report"""
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, number_of_months + 1):
+    for month in range(1, number_of_months +1):
         income = incomes[month - 1]
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        print("Month {:2} - Income: ${:10.2f}  Total: ${:10.2f}".format(month, income, total))
 
 
 main()
